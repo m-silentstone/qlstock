1425af2fcf921ed3e625ab60e0767c3e1fa8ecaa
liulei <371507051@qq.com>
2025-02-08 23:28:25 +0800

    黄金

--- Ashare/MyTestStrategy.py
+++ Ashare/MyTestStrategy.py
@@ -6,11 +6,13 @@ from  Ashare import *
 tax_ratio=0.9999
 fund=10000
 stock=0
-count=100
+count=250
 #df = get_price('sh000001', frequency='1d', count=count)  # 上证指数日线行情
-df = get_price('sh000300', frequency='1d', count=count)  # 日线行情
+#df = get_price('sh000300', frequency='1d', count=count)  # 沪深300日线行情
+df = get_price('sh518880', frequency='1d', count=count)  # 黄金ETF日线行情
 
-print('上证指数日线行情\n',df)
+
+print('日线行情\n',df)
 
 
 '''
@@ -28,6 +30,7 @@ for index, row in df.iterrows():
 
 '''
 
+'''
 #按定投天数平均分配金额，按收盘价买入
 day_fund=fund/count
 stock=0
@@ -36,6 +39,12 @@ for index, row in df.iterrows():
     print("close:",row['close'])
     stock+=day_fund/row['close']*tax_ratio
     print("stock:", stock, ",stock_fund:", stock*row['close']*tax_ratio)
+'''
 
 
+#第一天买入不动，最后一天卖出
+print(df.index[0],"收盘价：",df.iloc[0,3])
+print(df.index[count-1],"收盘价：",df.iloc[count-1,3])
+stock=fund/df.iloc[0,3]*tax_ratio
+print("stock:", stock, ",stock_fund:", stock*df.iloc[count-1,3]*tax_ratio)
 
