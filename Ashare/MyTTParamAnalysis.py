ce634bfe71b55e43541d6136b66a8abbabe3114e
liulei <371507051@qq.com>
2025-05-31 21:11:15 +0800

    調整統計程序

--- Ashare/MyTTParamAnalysis.py
+++ Ashare/MyTTParamAnalysis.py
@@ -4,14 +4,10 @@ import MyUtils;import time;
 import MyTT;
 from Ashare import *
 stock_count=10000
+stock_code_index_start = 2700
 #读全量股票文件
 allstockcode_array=[]
 # with open('all_stocks_basic.txt', 'r', encoding='utf-8') as file:
-#     lines = file.readlines()
-#     for line in lines:
-#         array=line.split()
-#         code_array=array[0].split('.')
-#         allstockcode_array.append(str(code_array[1]+code_array[0]).lower())
 with open('all_stocks_hk.txt', 'r', encoding='utf-8') as file:
     lines = file.readlines()
     for line in lines:
@@ -19,19 +15,19 @@ with open('all_stocks_hk.txt', 'r', encoding='utf-8') as file:
         code_array=array[0].split('.')
         allstockcode_array.append(str(code_array[1]+code_array[0]).lower())
 
-i=0
-for stock_code in allstockcode_array:
+stock_code_index = stock_code_index_start
+while stock_code_index < len(allstockcode_array):
     time.sleep(0.1)
-    i = i + 1
-    if i > stock_count:
+    print('stock_code_index:' + str(stock_code_index))
+    if stock_code_index >= stock_count:
         break
+    stock_code = allstockcode_array[stock_code_index];
     stock_map = MyUtils.get_from_gtime(stock_code)
-    if stock_map['pe'] <= 0 or stock_map['pe'] > 15 or stock_map['total_market_value'] < 1000:
-        continue
-    print(stock_code, stock_map['name'], stock_map['pe'], '--------------------------------------------------------')
-    print(stock_map)
-
-
+    if 0 < stock_map['pe'] <= 15 and stock_map['total_market_value'] >= 1000:
+        print(stock_code, stock_map['name'], stock_map['pe'], '--------------------------------------------------------')
+        print(stock_map)
+    stock_code_index = stock_code_index + 1
+print('程序运行结束...')
 
 
 
